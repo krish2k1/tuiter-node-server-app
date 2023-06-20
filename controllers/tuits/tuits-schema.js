a740c81a0813f959550f09c0468abebb0b733dab
kcreation <krishnan@kayatech.com>
2023-06-20 16:37:27 -0400

    mongo backend

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema(
+  {
+    topic: String,
+    userName: String,
+    title: String,
+    time: String,
+    image: String,
+    replies: Number,
+    retuits: Number,
+    tuit: String,
+    likes: Number,
+    handle: String,
+    liked: Boolean,
+  },
+  { collection: "tuits" }
+);
+export default schema;
--- users/users-dao.js
+++ users/users-dao.js
@@ -1,49 +1,27 @@
-import people from "./users.js";
+import usersModel from "./users-model.js";
 
-let users = people;
+export const findAllUsers = () => usersModel.find();
 
-export const findAllUsers = () => users;
-
-export const findUserById = (uid) => {
-  const index = users.findIndex((u) => u._id === uid);
-  if (index !== -1) return users[index];
-  return null;
+export const findUserById = (id) => {
+  usersModel.findById(id);
 };
 
 export const findUserByUsername = (username) => {
-  const index = users.findIndex((u) => u.username === username);
-  if (index !== -1) return users[index];
-  return null;
+  usersModel.findOne({ username });
 };
 
 export const findUserByCredentials = (username, password) => {
-  const index = users.findIndex(
-    (u) => u.username === username && u.password === password
-  );
-  if (index !== -1) return users[index];
-  return null;
+  usersModel.findOne({ username, password });
 };
 
 export const createUser = (user) => {
-  const newUser = { ...user, _id: new Date().getTime().toString() };
-  users.push(newUser);
-  return newUser;
+  usersModel.create(user);
 };
 
-export const updateUser = (uid, user) => {
-  const index = users.findIndex((u) => u._id === uid);
-  if (index !== -1) {
-    users[index] = { ...users[index], ...user };
-    return users[index];
-  }
-  return null;
+export const updateUser = (id, user) => {
+  usersModel.updateOne({ _id: id }, { $set: user });
 };
 
-export const deleteUser = (uid) => {
-  const index = users.findIndex((u) => u._id === uid);
-  if (index !== -1) {
-    users.splice(index, 1);
-    return { status: "ok" };
-  }
-  return null;
+export const deleteUser = (id) => {
+  usersModel.deleteOne({ _id: id });
 };
